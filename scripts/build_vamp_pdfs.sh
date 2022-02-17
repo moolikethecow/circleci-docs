11a47051137db03298cf105f8621be13b577bafc
Rosie Yohannan <rosie@circleci.com>
2022-02-17 21:43:03 +0000

    duplicate style dir and fix pointer to assets

--- scripts/build_vamp_pdfs.sh
+++ scripts/build_vamp_pdfs.sh
@@ -9,5 +9,5 @@ COMMENT="FINAL"
 # Build Overview
 echo "Building Vamp docs..."
 asciidoctor-pdf -v
-asciidoctor-pdf -a pdf-style=jekyll/_cci2/_release-pdf/circle-theme.yml -a pdf-fontsdir=jekyll/_cci2/_release-pdf/fonts -a skip-front-matter -a pdf=true -a revdate=${DATE} -D ${OUT_DIR} -o Vamp-Overview.pdf jekyll/_cci2/_continuous_release/index.adoc
+asciidoctor-pdf -a pdf-style=jekyll/_cci2/_continuous_release/_release-pdf/circle-theme.yml -a pdf-fontsdir=jekyll/_cci2/_continuous_release/_release-pdf/fonts -a skip-front-matter -a pdf=true -a revdate=${DATE} -D ${OUT_DIR} -o Vamp-Overview.pdf jekyll/_cci2/_continuous_release/index.adoc
 echo "Done!"
