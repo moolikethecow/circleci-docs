c95bc2aeac059620c566786f3233f760b3e33e71
teesloane <tylersloane@gmail.com>
2022-02-28 14:43:13 -0500

    Start: experiment for signup cta on first-steps.md

--- src/js/experiments/index.js
+++ src/js/experiments/index.js
@@ -1,10 +1,12 @@
 import forceAll from './forceAll';
 import kbLinks from './kbLinks';
 import languageGuides from './languageGuides';
+import signupLinksCta from './signupLinksCta';
 import './snippetFeedback';
 
 export default {
   forceAll,
   languageGuides,
   kbLinks,
+  signupLinksCta
 };
